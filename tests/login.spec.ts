ff6c84afcb4d4f3def2671b681b7b4805bbf642e
nRok89 <bartosz.wozniak89@onet.pl>
2023-07-17 09:47:03 +0200

    Day2

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,51 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://demo-bank.vercel.app/');
-  await page.getByTestId('login-input').click();
-  await page.getByTestId('login-input').fill('Login123');
-  await page.getByTestId('password-input').click();
-  await page.getByTestId('password-input').fill('Qwertyu8');
-  await page.getByTestId('login-button').click();
-  await page.getByTestId('user-name').click();
 
-  await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy') 
-});
+test.describe("User login to Bank", () => {
+
+  test('login with correct credentials', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('Login123');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('Qwertyu8');
+    await page.getByTestId('login-button').click();
+    await page.getByTestId('user-name').click();
+
+    // assercja
+  
+    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy')
+  });
+
+  test('unsuccessful login with to short login name', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('Login');
+
+    await page.getByTestId('password-input').fill('Qwertyu8');
+    await page.getByTestId('password-input').click();
+
+    // assercja
+    await expect(page.locator('#error_login_id')).toHaveText('identyfikator ma min. 8 znaków')
+  });
+
+
+  test('Unsuccessful login with to short password', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('Login123');
+
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('Qwerty');
+    await page.getByTestId('password-input').blur();
+
+  
+    // assercja
+    await expect(page.locator('#error_login_password')).toHaveText('hasło ma min. 8 znaków')
+    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków')
+
+  });
+
+})
